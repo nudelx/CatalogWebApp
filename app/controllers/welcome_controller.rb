e0c242097d45f4e54e9e72be2b15f436cd9b085f
nudelx <alex.nudelman@samanage.com>
2015-12-07 15:34:25 +0200

    inint

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+	# before_filter :find_model
+
+	def home
+				
+	end	
+
+	def about
+		
+	end
+
+	# private
+	# def find_model
+	# 	@model = Welcome.find(params[:id]) if params[:id]
+	# end
+end
